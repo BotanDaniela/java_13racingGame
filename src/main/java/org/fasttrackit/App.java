7f9b10a877d963d038962684341f9d2b82900d2d
Botan Daniela <botandaniela70@yahoo.com>
2019-09-02 21:02:45 +0300

    added constraint to  avoid having AutoVehicle without engine(studied constructors)

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -8,8 +8,14 @@ public class App
 {
     public static void main( String[] args )
     {
+        Engine engine = new Engine();
+        engine.manufacturer = "Renault";
+        engine.capacity = 1600;
+
+
+
         // this is a one-line comment
-        Car car = new Car();
+        Car car = new Car(engine);
         car.name = "Dacia";
         car.fuelLevel = 60;
         car.mileage = 8.5;
@@ -17,11 +23,6 @@ public class App
         car.maxSpeed = 180;
         car.fuelType = "Gasoline";
 
-        Engine engine = new Engine();
-        engine.manufacturer = "Renault";
-        engine.capacity = 1600;
-
-        car.engine = engine;
 
         System.out.println(car.name);
         System.out.println(car.running);
@@ -30,7 +31,11 @@ public class App
 
         car.accelerate(60, 1);
 
-        Car car2 = new Car();
+        Engine engine2 = new Engine();
+        engine2.manufacturer = "VW";
+        engine2.capacity = 1600;
+
+        Car car2 = new Car(engine2);
         car2.name = "Golf";
         car.fuelLevel = 70;
         car.doorCount = 2;
